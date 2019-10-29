13164474f49c2be1985bd6a0ebefb2c330947fb2
Nate <54870982+natepmeyer@users.noreply.github.com>
2019-10-28 22:54:16 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+    createCanvas(600, 400);
+  }
+  
+  function draw() {
+    background(100,32,29);
+    fill(123,23,129);
+    circle(285,120,80);
+    circle(270,120,10);
+    circle(300,120,10);
+    fill(123,123,129);
+    square(200,200,50);
+    square(320,200,50);
+    square(300,300,50);
+    square(220,300,50);
+    square(220,320,50)
+    square(220,350,50)
+    square(300,320,50);
+    square(300,350,50);
+    fill(23,223,29);
+    fill(223,123,29);
+    rect(220,160,130,160);
+    rect(150,90,50,300);
+    rect(90,90,50,300);
+    fill(0);
+    textSize(32);
+    text("Nature-Boy", 100,40);
+    text("SKi oR DiE", 330,40);
+    text("Nate Meyer", 370,200);
+  }
